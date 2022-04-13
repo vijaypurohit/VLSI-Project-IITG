6fc56f85fd8b9375537917f44127adceee0db396
Vijay Purohit <vijay.pu9@gmail.com>
2022-04-13 20:05:50 +0530

    paper 1st section

---
+++ Tutorial/matrixmul.cpp
@@ -0,0 +1,26 @@
+
+
+#include "matrixmul.h"
+
+void matrixmul(
+      mat_a_t a[MAT_A_ROWS][MAT_A_COLS],
+      mat_b_t b[MAT_B_ROWS][MAT_B_COLS],
+      result_t res[MAT_A_ROWS][MAT_B_COLS])
+{
+  // Iterate over the rows of the A matrix
+  Row: for(int i = 0; i < MAT_A_ROWS; i++) {
+    // Iterate over the columns of the B matrix
+    Col: for(int j = 0; j < MAT_B_COLS; j++) {
+
+
+    	#pragma HLS PIPELINE II=1
+
+      res[i][j] = 0;
+      Product: for(int k = 0; k < MAT_B_ROWS; k++) {
+        res[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+}
+
+
